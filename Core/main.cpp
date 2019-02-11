714300b65f46b8ac9181a259b4a5a14a67e21e52
Jonasz <jonaszromanowski@gmail.com>
2019-02-11 16:35:32 +0100

    Added class responsible for reading files

--- Core/main.cpp
+++ Core/main.cpp
@@ -1,5 +1,6 @@
 #include "src/graphics/window.h"
 #include "src/maths/maths.h"
+#include "src/utils/fileutils.h"
 
 int main () {
 
@@ -10,20 +11,14 @@ int main () {
 	Window window ("Sparky!", 800, 600);
 	glClearColor (0.2f, 0.3f, 0.8f, 1.0f);
 
+	std::string main_file = read_file ("main.cpp");
+	std::cout << main_file << std::endl;
+	system ("PAUSE");
 
 	GLuint vao;
 	glGenVertexArrays (1, &vao);
 	glBindVertexArray (vao);
 
-	vec4 vector (0.2f, 0.3f, 0.8f, 1.0f);
-	vec4 vector1 (0.5f, 0.2f, 0.1f, 1.0f);
-	vec4 result = vector + vector1;
-
-	mat4 position = mat4::translation (vec3 (2, 3, 4));
-
-	vec4 column4 = position.columns[3];
-
-	std::cout << column4 << std::endl;
 
  	while (!window.Closed ()) {
 
